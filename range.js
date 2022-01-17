f76a9284ca9dcb461237b68c26fe3af5303220a4
grjan7 <94439978+grjan7@users.noreply.github.com>
2022-01-17 19:58:19 +0530

    created range.js

---
+++ range.js
@@ -0,0 +1,21 @@
+/**
+ * @param rangeLen {number} length of range to be returned
+ * @returns an array of integer number begins from zero
+ * @author Jana R <janagr7@gmail.com>
+ */
+function range(rangeLen) {
+  let array = [];
+  for (let i = 0; i < rangeLen; i++) {
+    array.push(i)
+  }
+  return array;
+}
+
+module.exports = range;
+
+
+
+//example
+
+range(10);
+
